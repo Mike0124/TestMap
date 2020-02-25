c4764f03963e5de92495c52436c67c5202d03c0a
Hyq0719 <2418011730@qq.com>
2020-02-25 22:30:43 +0800

    规范代码格式 添加按键动态效果

--- app/src/main/java/com/example/testmap/MainActivity.java
+++ app/src/main/java/com/example/testmap/MainActivity.java
@@ -10,8 +10,6 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 
@@ -24,73 +22,20 @@ import com.yzq.zxinglibrary.android.CaptureActivity;
 import com.yzq.zxinglibrary.bean.ZxingConfig;
 import com.yzq.zxinglibrary.common.Constant;
 
-public class MainActivity extends AppCompatActivity{
-    private Button mscanbutton;
-    private ImageButton mloginbutton;
-    MapView mapView = null;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private int REQUEST_CODE_SCAN = 1;
+    MapView mapView = null;
+    private Bundle savedInstanceState;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//设置对应的XML布局文件
-        mapView = (MapView)findViewById(R.id.map);
-        mapView.onCreate(savedInstanceState);// 此方法必须重写
-        AMap aMap;
-            aMap = mapView.getMap();
-        //设置地图的放缩级别
-        aMap.moveCamera(CameraUpdateFactory.zoomTo(13));
-        MyLocationStyle myLocationStyle;
-//初始化定位蓝点样式类
-        myLocationStyle = new MyLocationStyle();
-//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
-        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
-//设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-        myLocationStyle.interval(2000);
-//设置定位蓝点的Style
-        aMap.setMyLocationStyle(myLocationStyle);
-//设置默认定位按钮是否显示，非必需设置。
-        aMap.getUiSettings().setMyLocationButtonEnabled(true);
-// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
-        aMap.setMyLocationEnabled(true);
-        mloginbutton =  findViewById(R.id.Login);
-        mloginbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
-        mscanbutton = findViewById(R.id.scan_button);
-        mscanbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            if(Build.VERSION.SDK_INT > 22) {        //判断手机版本是否在6.0以上，如在以上则需要动态申请权限
-                if(ContextCompat.checkSelfPermission(MainActivity.this,
-                        Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{Manifest.permission.CAMERA}, 1);
-                }
-                else{
-                    Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
-                    ZxingConfig config = new ZxingConfig();
-                    config.setShowAlbum(false); //是否显示相册
-                    config.setFullScreenScan(false);//是否全屏扫描  默认为true  设为false则只会在扫描框中扫描
-                    intent.putExtra(Constant.INTENT_ZXING_CONFIG, config);
-                    startActivityForResult(intent, REQUEST_CODE_SCAN);
-                }
-            }
-            else {
-                Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
-                ZxingConfig config = new ZxingConfig();
-                config.setShowAlbum(false); //是否显示相册
-                config.setFullScreenScan(false);//是否全屏扫描  默认为true  设为false则只会在扫描框中扫描
-                intent.putExtra(Constant.INTENT_ZXING_CONFIG, config);
-                startActivityForResult(intent, REQUEST_CODE_SCAN);
-            }
-            }
-        });
-
+        iniView();//初始化地图布局
+        findViewById(R.id.scan_button).setOnClickListener(this);//扫描二维码
+        findViewById(R.id.Login).setOnClickListener(this);//登录
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -98,34 +43,88 @@ public class MainActivity extends AppCompatActivity{
             // 扫描二维码/条码回传
             if (requestCode == REQUEST_CODE_SCAN && resultCode == RESULT_OK) {
                 String content = data.getStringExtra(Constant.CODED_CONTENT);
-                Toast.makeText(this, "扫码成功，结果："+content, Toast.LENGTH_SHORT).show();
-                }
+                Toast.makeText(this, "扫码成功，结果：" + content, Toast.LENGTH_SHORT).show();
             }
         }
+    }
 
     @Override
-    protected  void onResume(){
+    protected void onResume() {
         super.onResume();       //重新绘制加载地图
         mapView.onResume();
     }
+
     @Override
-    protected  void onPause(){
+    protected void onPause() {
         super.onPause();        //暂停地图的绘制
         mapView.onPause();
     }
+
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
     }
+
     @Override
-    protected void onSaveInstanceState(Bundle outState){
+    protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);      //保存地图
         mapView.onSaveInstanceState(outState);
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         //可在此继续其他操作。
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.scan_button:
+                ZxingConfig config = new ZxingConfig();
+                config.setShowAlbum(false); //是否显示相册
+                config.setFullScreenScan(false);//是否全屏扫描  默认为true  设为false则只会在扫描框中扫描
+                if (Build.VERSION.SDK_INT > 22) {        //判断手机版本是否在6.0以上，如在以上则需要动态申请权限
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.CAMERA}, 1);
+                    } else {
+                        Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                        intent.putExtra(Constant.INTENT_ZXING_CONFIG, config);
+                        startActivityForResult(intent, REQUEST_CODE_SCAN);
+                    }
+                } else {
+                    Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                    intent.putExtra(Constant.INTENT_ZXING_CONFIG, config);
+                    startActivityForResult(intent, REQUEST_CODE_SCAN);
+                }
+            case R.id.Login:
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+        }
+    }
+
+    public void iniView() {
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        AMap aMap;
+        aMap = mapView.getMap();
+        //设置地图的放缩级别
+        aMap.moveCamera(CameraUpdateFactory.zoomTo(13));
+        MyLocationStyle myLocationStyle;
+        //初始化定位蓝点样式类
+        myLocationStyle = new MyLocationStyle();
+        //连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+        //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.interval(2000);
+        //设置定位蓝点的Style
+        aMap.setMyLocationStyle(myLocationStyle);
+        //设置默认定位按钮是否显示，非必需设置。
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);
+        // 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        aMap.setMyLocationEnabled(true);
     }
 }
