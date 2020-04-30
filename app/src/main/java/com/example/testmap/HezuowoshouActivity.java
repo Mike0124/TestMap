f490237a18902ba5d051ed89e6393e9e46b98b61
Mike0124 <54769858+Mike0124@users.noreply.github.com>
2020-04-30 17:11:44 +0800

    添加了"加盟合作"的页面、实现智小盖后台调用

--- app/src/main/java/com/example/testmap/HezuowoshouActivity.java
+++ app/src/main/java/com/example/testmap/HezuowoshouActivity.java
@@ -1,57 +1,93 @@
 package com.example.testmap;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Looper;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedReader;
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Objects;
 
-import static com.amap.api.maps.model.BitmapDescriptorFactory.getContext;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 
 public class HezuowoshouActivity extends AppCompatActivity {
+
+    private static String string_url;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hezuowoshou);
-        TextView textView = findViewById(R.id.hezuowoshou_text);
-        textView.setText(gethttpresult("http://zhixiaogai.com/api/query_league_cooperation"));
-    }
-
-    public static String gethttpresult(String urlStr) {
+        imageView = findViewById(R.id.hezuowoshou_text);
+        Thread subThread = new Thread(new SubThread());
+        subThread.start();
         try {
-            URL url = new URL(urlStr);
-            HttpURLConnection connect = (HttpURLConnection) url.openConnection();
-            connect.setRequestMethod("GET");
-            connect.setConnectTimeout(5000);
-            connect.setReadTimeout(5000);
-            connect.connect();
-            if (connect.getResponseCode() == 200) {
-                InputStream input = connect.getInputStream();
-                BufferedReader in = new BufferedReader(new InputStreamReader(input));
-                String line = null;
-                System.out.println(connect.getResponseCode());
-                StringBuffer sb = new StringBuffer();
-                while ((line = in.readLine()) != null) {
-                    sb.append(line);
-                }
-                return sb.toString();
-            } else {
-                return "meiyou";
-            }
-        } catch (Exception e) {
-            System.out.println(e.toString());
-            return "meiyou222";
+            subThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
+    private class SubThread implements Runnable{
+        public void run(){
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("https://zhixiaogai.com/api/query_league_cooperation")
+                    .get()
+                    .addHeader("Content-Type","application/x-www-form-urlencoded")
+                    .build();
+            try {
+                Response response = client.newCall(request).execute();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    String string = Objects.requireNonNull(response.body()).string();
+                    new JSONObject();
+                    JSONObject JSONObject_hezuowoshou;
+                    JSONObject_hezuowoshou = JSONObject.parseObject(string);
+                    string_url = JSONObject_hezuowoshou.getString("img_url");
+                    Bitmap bitmap = getHttpBitmap(string_url);
+                    imageView.setImageBitmap(bitmap);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    protected static Bitmap getHttpBitmap(String url){
+        URL myFileURL;
+        Bitmap bitmap = null;
+        try{
+            myFileURL = new URL(url);
+            //获得连接
+            HttpURLConnection conn=(HttpURLConnection)myFileURL.openConnection();
+            //设置超时时间为6000毫秒，conn.setConnectionTiem(0);表示没有时间限制
+            conn.setConnectTimeout(6000);
+            //连接设置获得数据流
+            conn.setDoInput(true);
+            //不使用缓存
+            conn.setUseCaches(false);
+            //这句可有可无，没有影响
+            //conn.connect();
+            //得到数据流
+            InputStream is = conn.getInputStream();
+            //解析得到图片
+            bitmap = BitmapFactory.decodeStream(is);
+            //关闭数据流
+            is.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
 }
